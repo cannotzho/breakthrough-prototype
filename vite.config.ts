80a811af47ae9950bf2e866acff5a795247ff5b9
Breakthrough Dev <dev@breakthrough.local>
2026-07-06 23:10:56 +0800

    cd: update vite config manually

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/breakthrough-prototype/dev/',
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    strictPort: false,
+  },
+})
